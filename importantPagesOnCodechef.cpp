ccacec9a927708dd50614ab5154a61452894ea4a
Sapnendra Jaiswal <116286507+Sapnendra@users.noreply.github.com>
2023-02-19 19:48:45 +0530

    Add files via upload

---
+++ importantPagesOnCodechef.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	
+	if(a==0) {
+	    cout<<"https://www.codechef.com/practice"<<endl;
+	} else if(b==0) {
+	    cout<<"https://www.codechef.com/contests"<<endl;
+	} else {
+	    cout<<"https://discuss.codechef.com"<<endl;
+	}
+	return 0;
+}
